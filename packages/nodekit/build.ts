8806bb7d15fb398270d8f9d48b3bda9e5b7fd8f5
Kingsword <kingsword09@gmail.com>
2025-05-08 17:09:45 +0800

    🎨 modify build

--- packages/nodekit/build.ts
+++ packages/nodekit/build.ts
@@ -7,6 +7,11 @@ if (import.meta.main) {
     format: "both",
     platform: "node",
     external: ["@std/jsonc", "quansync"],
-    packageJson: { author: "Kingsword kingsword09 <kingsword09@gmail.com>" },
+    packageJson: {
+      author: "Kingsword kingsword09 <kingsword09@gmail.com>",
+      repository: { "type": "git", "url": "git+https://github.com/kingsword09/std.git" },
+      homepage: "https://github.com/kingsword09/std#readme",
+      license: "Apache-2.0 or MIT",
+    },
   });
 }
