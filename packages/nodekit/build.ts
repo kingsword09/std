aa56bec2b3968a0e95605169011d2dc5612a62e9
Kingsword <kingsword09@gmail.com>
2025-05-08 16:59:41 +0800

    🐛 [nodekit] fix build error

--- packages/nodekit/build.ts
+++ packages/nodekit/build.ts
@@ -2,10 +2,11 @@ import { build } from "jsr:@kingsword09/dwpkg";
 
 if (import.meta.main) {
   await build({
-    config: import.meta.resolve("./deno.json"),
+    denoJsonPath: import.meta.resolve("./deno.json"),
     jsrRegistry: false,
     format: "both",
     platform: "node",
     external: ["@std/jsonc", "quansync"],
+    packageJson: { author: "Kingsword kingsword09 <kingsword09@gmail.com>" },
   });
 }
